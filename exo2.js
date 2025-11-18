5e37b94b2466bab54c7df8dcab9a29f2d800e843
lydiaalm4 <146334119+lydiaalm4@users.noreply.github.com>
2025-11-18 21:09:08 +0100

    Fix comments formatting and reorder conditions

--- exo2.js
+++ exo2.js
@@ -1,29 +1,21 @@
-// 1. The first n elements of an array
+// 1/ The first n elements of an array
 function first(array, n) {
   if (array == null) return [];
-
-  if (n == null) return array[0];   // FIXED (must be before n <= 0)
-
+  if (n == null) return array[0];   
   if (n <= 0) return [];
-
   return array.slice(0, n);
 }
-
-// 2. The last n elements of an array
+// 2/ The last n elements of an array
 function last(array, n) {
   if (array == null) return [];
-
   if (n == null) return array[array.length - 1];
-
   return array.slice(Math.max(array.length - n, 0));
 }
-
-// 3. String = concatenation of all strings in array
+// 3/ String = concatenation of all strings in array
 function joinStrings(arr) {
   return arr.join('');
 }
-
-// 4. Divide an array into several sub-arrays of predefined size
+// 4/ Divide an array into several sub-arrays of predefined size
 function chunk(array, size) {
   var chunkedArr = [];
   var index = 0;
@@ -33,5 +25,4 @@ function chunk(array, size) {
   }
   return chunkedArr;
 }
-
 module.exports = { first, last, joinStrings, chunk };
